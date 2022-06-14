3562cf649731541d52ddd1c09c15891f7f065be2
aule2000 <aukse.lev6@gmail.com>
2022-06-14 20:02:27 +0300

    test updated

--- BackEnd/src/test/java/com/example/controllers/PersonControllerITTest.java
+++ BackEnd/src/test/java/com/example/controllers/PersonControllerITTest.java
@@ -8,33 +8,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest(classes = TaskApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = TaskApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 public class PersonControllerITTest {
     @LocalServerPort
     private int port;
 
-
     @Autowired
     PersonServiceImpl service;
-
 
     @Test
     void test() {
         System.out.println("PORT=" + port);
     }
+
     @Test
-    void testFindPersonByIdAndBirthDate() throws Exception{
-        String expected = "[{\"id\":34,\"personalCode\":99999999999,\"firstName\":\"bla\",\"lastName\":\"sda\",\"gender\":\"Male\",\"birthDate\":1654549200000}]";
-                WebTestClient
+    void testFindPersonByIdAndBirthDate() throws Exception {
+
+        String expected = "[{\"personalCode\":47584125874,\"firstName\":\"John\",\"lastName\":\"Depp\",\"gender\":\"Male\",\"birthDate\":1654549200000}]";
+        WebTestClient
                 .bindToServer()
                 .baseUrl("http://localhost:"+port)
                 .build()
                 .get()
-                .uri("/person/99999999999/2022-06-07")
+                .uri("/person/47584125874/2022-06-07")
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().valueEquals("Content-Type", "application/json")
                 .expectBody().json(expected);
 
     }
+
+
+
 }
